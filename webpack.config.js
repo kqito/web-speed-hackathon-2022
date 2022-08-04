c2e5c72517f8752b6549677235edbba6025240bc
kqito <29191111+kqito@users.noreply.github.com>
2022-08-04 18:25:43 +0900

    fix webpack
    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require("path");
 
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const safePostCssParser = require("postcss-safe-parser");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const nodeExternals = require("webpack-node-externals");
 
 function abs(...args) {
@@ -13,14 +22,91 @@ const PUBLIC_ROOT = abs("./public");
 const DIST_ROOT = abs("./dist");
 const DIST_PUBLIC = abs("./dist/public");
 
+const createBase = ({ isClient }) => ({
+  devtool: false,
+  optimization: {
+    minimize: isClient,
+    minimizer: isClient
+      ? [new TerserPlugin(), new CssMinimizerPlugin()]
+      : undefined,
+    splitChunks: {
+      cacheGroups: {
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        defaultVendors: {
+          priority: -10,
+          reuseExistingChunk: true,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+      chunks: "all",
+      enforceSizeThreshold: 50000,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      minChunks: 1,
+      minRemainingSize: 0,
+      minSize: 20000,
+    },
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
+    }),
+    new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(ja)$/),
+    process.env.ANALYZER === "true" &&
+      new BundleAnalyzerPlugin({
+        analyzerPort: isClient ? 8887 : 8889,
+      }),
+    new webpack.ProvidePlugin({
+      // $: 'jquery',
+      // AudioContext: ['standardized-audio-context', 'AudioContext'],
+      Buffer: ["buffer", "Buffer"],
+      // 'window.jQuery': 'jquery',
+    }),
+    new webpack.EnvironmentPlugin({
+      BUILD_DATE: new Date().toISOString(),
+      // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
+      COMMIT_HASH: process.env.SOURCE_VERSION || "",
+      NODE_ENV: "production",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].css",
+    }),
+    // new MiniCssExtractPlugin(), こっちかも
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+      template: path.resolve(SRC_ROOT, "./templates/index.html"),
+    }),
+  ].filter(Boolean),
+});
+
 /** @type {Array<import('webpack').Configuration>} */
 module.exports = [
   {
-    devtool: "inline-source-map",
+    ...createBase({ isClient: true }),
     entry: path.join(SRC_ROOT, "client/index.jsx"),
-    mode: "development",
+    mode: "production",
     module: {
       rules: [
+        // {
+        //   test: /.s?css$/,
+        //   use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        // },
         {
           resourceQuery: (value) => {
             const query = new URLSearchParams(value);
@@ -29,17 +115,26 @@ module.exports = [
           type: "asset/source",
         },
         {
-          exclude: /[\\/]esm[\\/]/,
+          exclude: /node_modules/,
           test: /\.jsx?$/,
           use: {
             loader: "babel-loader",
             options: {
+              plugins: [
+                [
+                  "babel-plugin-styled-components",
+                  {
+                    pure: true,
+                  },
+                ],
+              ],
               presets: [
                 [
                   "@babel/preset-env",
                   {
-                    modules: "cjs",
-                    spec: true,
+                    // spec: true,
+                    loose: true,
+                    modules: false,
                   },
                 ],
                 "@babel/preset-react",
@@ -53,21 +148,20 @@ module.exports = [
     output: {
       path: DIST_PUBLIC,
     },
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
-      }),
-    ],
+    // output: {
+    //   filename: "[name].js",
+    //   path: DIST_ROOT,
+    // },
     resolve: {
       extensions: [".js", ".jsx"],
     },
     target: "web",
   },
   {
-    devtool: "inline-source-map",
+    ...createBase({ isClient: false }),
     entry: path.join(SRC_ROOT, "server/index.js"),
     externals: [nodeExternals()],
-    mode: "development",
+    mode: "production",
     module: {
       rules: [
         {
@@ -76,12 +170,21 @@ module.exports = [
           use: {
             loader: "babel-loader",
             options: {
+              plugins: [
+                [
+                  "babel-plugin-styled-components",
+                  {
+                    pure: true,
+                  },
+                ],
+              ],
               presets: [
                 [
                   "@babel/preset-env",
                   {
+                    loose: true,
                     modules: "cjs",
-                    spec: true,
+                    // spec: true,
                   },
                 ],
                 "@babel/preset-react",
@@ -93,7 +196,7 @@ module.exports = [
     },
     name: "server",
     output: {
-      filename: "server.js",
+      filename: "[name].js",
       path: DIST_ROOT,
     },
     resolve: {
